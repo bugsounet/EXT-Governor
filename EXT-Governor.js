0517e69889da8fd8495220f74e37a5d95890a1da
bugsounet <github@bugsounet.fr>
2022-02-26 22:58:59 +0100

    delete spaces

--- EXT-Governor.js
+++ EXT-Governor.js
@@ -5,32 +5,31 @@
 ******************/
 
 Module.register("EXT-Governor", {
-    requiresVersion: "2.18.0",
-    defaults: {
-      debug: true,
-      sleeping: "powersave",
-      working: "ondemand"
-    },
+  requiresVersion: "2.18.0",
+  defaults: {
+    debug: false,
+    sleeping: "powersave",
+    working: "ondemand"
+  },
 
-    notificationReceived: function (notification, payload) {
-      switch(notification) {
-        case "DOM_OBJECTS_CREATED":
-          this.sendSocketNotification("INIT", this.config)
-          this.sendNotification("EXT_HELLO", this.name)
-          break
-        case "EXT_GOVERNOR-WORKING":
-          this.sendSocketNotification("WORKING")
-          break
-        case "EXT_GOVERNOR-SLEEPING":
-          this.sendSocketNotification("SLEEPING")
-          break
-      }
-    },
+  notificationReceived: function (notification, payload) {
+    switch(notification) {
+      case "DOM_OBJECTS_CREATED":
+        this.sendSocketNotification("INIT", this.config)
+        this.sendNotification("EXT_HELLO", this.name)
+        break
+      case "EXT_GOVERNOR-WORKING":
+        this.sendSocketNotification("WORKING")
+        break
+      case "EXT_GOVERNOR-SLEEPING":
+        this.sendSocketNotification("SLEEPING")
+        break
+    }
+  },
 
-    getDom: function () {
-      var dom = document.createElement("div")
-      dom.style.display = 'none'
-      return dom
-    },
-
+  getDom: function () {
+    var dom = document.createElement("div")
+    dom.style.display = 'none'
+    return dom
+  },
 });
